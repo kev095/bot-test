28d407fd7dc459e6da951366d3225c6c3f833830
KevinJoseph <kevramos95@gmail.com>
2022-11-02 16:52:19 -0500

    config(project): add eslit and prettier

--- src/server.ts
+++ src/server.ts
@@ -4,21 +4,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export class ServerWeb {
+  app: Express = express();
+  port: any;
 
-    app: Express = express();
-    port: any;
+  constructor() {
+    this.port = process.env.PORT;
+  }
 
-    constructor(){
-        this.port = process.env.PORT;
-    }
+  init() {
+    this.app.get('/', (req: Request, res: Response) => {
+      res.send('Express + TypeScript Server');
+    });
 
-    init(){
-        this.app.get('/', (req: Request, res: Response) => {
-            res.send('Express + TypeScript Server');
-          });
-
-        this.app.listen(this.port, () => {
-            console.log(`[server]: Server is running 🚀 at https://localhost:${this.port}`);
-          });
-    }
+    this.app.listen(this.port, () => {
+      console.log(`[server]: Server is running 🚀 at https://localhost:${this.port}`);
+    });
+  }
 }
